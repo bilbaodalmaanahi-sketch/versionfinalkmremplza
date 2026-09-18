7f40dab036b7c58e7c8648810385222564262e19
DAB <bilbaodalmaanahi@gmail.com>
2026-09-18 18:14:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ nuevo_km_input = st.number_input(
 # MARGEN DE BÚSQUEDA EN METROS
 # ============================================================
 
-MARGEN_BUSQUEDA_METROS = 1_100_000
+MARGEN_BUSQUEDA_METROS = 1_000_000
 
 st.number_input(
     "Margen de búsqueda en metros",
